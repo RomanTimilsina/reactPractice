861e711afef502e3701101ca7c5faf4f04a66141
Roman <romantimilsina@gmail.com>
2022-12-07 13:02:12 +0545

    A-basic-practice-app

--- src/App.js
+++ src/App.js
@@ -1,18 +1,67 @@
-import Todo from './Todo'
 import TodoList from './TodoList'
-import {useState} from "react"
+import {useState, useRef, useEffect} from "react"
+import { v4 as uuidV4 } from "uuid"
+
+const LOCAL_STORAGE_KEY = 'todoApp'
 
 function App() {
+  
+  const [todos, setTodos] = useState([])
+  const todoRef = useRef()
 
-  const [todos,setTodos] = useState(['Todo 1', 'Todo 2'])
+ /* useEffect(() => {
+    const storedTodos = JSON.parse(localStorage.getItem('todoApp.todos'))
+    setTodos(storedTodos)
+  },[])
+
+
+  useEffect(() => {
+    localStorage.setItem('todoApp.todos', JSON.stringify(todos))
+  },[todos])
+*/
+
+useEffect(() => {
+  const storedTodos = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY))
+  if (storedTodos) setTodos(storedTodos)
+  console.log(storedTodos)
+}, [])
+
+useEffect(() => {
+  localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(todos))
+  console.log((todos))
+}, [todos])
+  
+
+  
+  function handleAddTodo(e) {
+    const name = todoRef.current.value
+    if (name === '') return
+    setTodos(prevTodos => {
+      return [...prevTodos, { id: uuidV4(), name: name, completed: false}]
+    })
+    todoRef.current.value = null
+  }
+
+  function handleClearTodo(e) {
+   const newTodo = todos.filter(todo => !todo.completed)
+   setTodos(newTodo)
+  }
+
+  function toggleTodo(id){
+    const newTodo = [...todos]
+    let todo =  newTodo.find(todo => todo.id === id)
+    todo.completed = !todo.completed
+    setTodos(newTodo)
+
+  }
 
   return (
     <>
-      <TodoList todos = {todos} />
-      <input type="text" />
-      <button>Add Todo</button>
-      <button>Clear Complete</button>
-      <div>0 left to do</div>
+      <TodoList todos = {todos} toggleTodo = {toggleTodo} />
+      <input ref={todoRef} type="text" />
+      <button onClick = {handleAddTodo}>Add Todo</button>
+      <button onClick = {handleClearTodo}>Clear Complete</button>
+      <div>{(todos.filter(todo => !todo.completed).length)}</div>
     </>
   );
 }
--- src/Todo.jsx
+++ src/Todo.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
 
-export default function Todo({todo}) {
+export default function Todo({todo, toggleTodo}) {
+
+  function handleToggle() {
+      toggleTodo(todo.id)
+  }
+
   return (
     <div>
       <label>
-      <input type="checkbox" checked={todo.completed}/>
+      <input type="checkbox" checked={todo.completed} onChange = {handleToggle} />
       {todo.name}
       </label>
     </div>
